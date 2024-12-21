377473065168c12d6ce421b3d1f50b7a10ce79bc
GayLachner-Marc <mgayl@uoc.edu>
2024-12-21 20:07:21 +0100

    Major update Firestore & Messaging

--- app/App.tsx
+++ app/App.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import OurTabs from '@/components/TabBar/Navigation';
 import {PlayersComponent} from "@/components/PlayerCrudComponent/CrudComponent"; // Ruta correcta
-
+import {PermissionsAndroid} from 'react-native';
 const App = () => {
+  PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
   return (
     <NavigationContainer>
       <OurTabs /> {/* Aquí va tu Tab Navigator */}
